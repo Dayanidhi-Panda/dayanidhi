7f3ef2673db91161c14e0281ea7fa9945dc0dbdd
Dayanidhi-Panda <47910214+Dayanidhi-Panda@users.noreply.github.com>
2021-03-10 21:05:55 +0530

    Update Login.js

--- src/Component/Customer-module/Login.js
+++ src/Component/Customer-module/Login.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react'
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export class Login extends Component {
 
-   onSubmit = ()  =>{
-    alert('');
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.history.push('/product')
   }
 
-    render() {
-        return (
-            <div>
-                <form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-  </div>
-  <div className="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary" onClick={this.onSubmit}>Submit</button>
-</form>
-{/* <Link to='/Product'>
+  render() {
+    return (
+      <div>
+        <form>
+          <h3>Sign In</h3>
+
+          <div className="form-group">
+            <label>Email address</label>
+            <input type="email" className="form-control" placeholder="Enter email" />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Enter password" />
+          </div>
+
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="customCheck1" />
+              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            </div>
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-block" onClick={this.onSubmit}>Submit</button>
+          <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+          </p>
+        </form>
+
+        {/* <Link to='/Product'>
 <button >Btn</button>
 </Link> */}
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default Login
